e56f38c6dceb6ba7a806f5fe65e7c759342727f5
Chet Jogia <chetjogia@gmail.com>
2023-01-18 23:46:48 +0000

    worked on CSS for cards and updated two projects

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,35 +1,47 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import projectImage1 from "../../images/chetjogiasiteimage.png";
 import styles from "./Card.module.css";
-import {FaGithub} from "react-icons/fa"
-import {MdOutlineLaunch} from "react-icons/md"
-import {IoConstructOutline} from 'react-icons/io5'
-import {RxDoubleArrowUp} from 'react-icons/rx'
+import { FaGithub } from "react-icons/fa";
+import { MdOutlineLaunch } from "react-icons/md";
+import { IoConstructOutline } from "react-icons/io5";
+import { RxDoubleArrowUp } from "react-icons/rx";
+import Link from "next/link";
 
-function Card() {
+interface Project {
+  projectHeader: string;
+  projectBody: string;
+  techStack:string;
+  image: StaticImageData;
+  github: string;
+  website:string;
+}
+
+function Card(props: Project) {
   return (
     <div className={styles["card-content"]}>
       <div className={styles["image-div"]}>
-        <IoConstructOutline size="auto" className={styles["construction"]}/>
-{/*       <Image src={projectImage1} alt="project1" className={styles["card-image"]} />  */}
+     {/*    <IoConstructOutline size="auto" className={styles["construction"]} /> */}
+              <Image src={props.image} alt="project1"  style={{objectFit:"cover"}}/> 
       </div>
 
       <div className={styles["card-body"]}>
         <div className={styles["text"]}>
           <div className={styles["project-header-div"]}>
-          <h3 className={styles["project-header"]}>Project Coming Soon</h3> <RxDoubleArrowUp size="auto" className={styles["uparrow"]}/>
+            <h3 className={styles["project-header"]}>Project: {props.projectHeader}</h3>{" "}
+            <RxDoubleArrowUp size="auto" className={styles["uparrow"]} />
           </div>
-    
-        <p className={styles["project-body"]}>
-         Coming soon
-        </p>  
 
+          <p className={styles["project-body"]}>{props.projectBody}</p>
+          <p className={styles["project-tech"]}><i>Tech Stack:</i> {props.techStack}</p>
         </div>
-     
+
         <div className={styles["button-container"]}>
-          <button className={styles["open-git"]}><FaGithub size="auto" className={styles["icon"]}/></button>
-          <button className={styles["open-site"]}><MdOutlineLaunch size="auto" className={styles["icon"]}/></button>
-     
+          <Link target="_blank" href={props.github} className={styles["open-git"]}>
+            <FaGithub size="auto" className={styles["icon"]} />
+          </Link>
+          <Link target="_blank" href={props.website} className={styles["open-site"]}>
+            <MdOutlineLaunch size="auto" className={styles["icon"]} />
+          </Link>
         </div>
       </div>
     </div>
--- components/CardContainer/CardContainer.tsx
+++ components/CardContainer/CardContainer.tsx
@@ -1,11 +1,36 @@
 import Card from "../Card/Card";
-import styles from "./CardContainer.module.css"
+import styles from "./CardContainer.module.css";
+import utellum from "../../images/utellum_screenshot.png";
+import homegrown from "../../images/homegrown_screenshot.png"
+import Image, { StaticImageData } from "next/image";
+
+
+interface Project{
+  projectHeader: string;
+  projectBody:string;
+  projectSubheader:string;
+  techStack:string
+  image: StaticImageData;
+  github: String;
+  website: String;
+}
+
 function CardContainer() {
-    return ( <div className={styles["card-container"]}>
-        <Card/>
-        <Card/>
-        <Card/>
-    </div> );
+    let utellumProject =  {projectHeader: "Utellum", projectSubheader:"Peer-2-Peer Learning Platform", projectBody: "A one-week project with a brief to develop a web application that would benefit current and future boot-campers.", techStack:"React, Express, Node, postgreSQL, Jest, Supertest, React Testing Library, CSS", image: utellum, github: "https://github.com/chetjogia/bc13_w9_project-frontend-devindy", website:"https://shiny-lamington-ec7ea1.netlify.app/"};
+
+
+    let monthProject =  {projectHeader: "Home-Grown", projectSubheader:"Urban Farming Community", projectBody: "Connecting land owners with people who'd like to farm tackling the cost of living crisis and sustainability", techStack:"Next.JS, Express, Node, postgreSQL, Jest, Supertest, Cypress, React Testing Library, CSS, Firebase", image: homegrown, github:"https://github.com/chetjogia/home-grown-FE", website:"https://home-grown.vercel.app/"};
+
+
+
+
+  return (
+    <div className={styles["card-container"]}>
+      <Card {...utellumProject} />
+      <Card {...monthProject}/>
+     {/*  <Card /> */}
+    </div>
+  );
 }
 
 export default CardContainer;
